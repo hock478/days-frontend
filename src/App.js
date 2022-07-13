7a6b292dc97f718f4b972eae08a58868952e1559
Fieraol Shiferaw <fieraolshiferaw@fieraols-mbp.myfiosgateway.com>
2022-07-12 23:21:50 -0400

    YAYA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginComponent from './components/LoginComponent/LoginComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          PagMan juicer.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+      //   <div className="App">
+      //     <header className="App-header">
+      //       <img src={logo} className="App-logo" alt="logo" />
+      //       <p>
+      //         PagMan juicer.
+      //       </p>
+      //       <a
+      //         className="App-link"
+      //         href="https://reactjs.org"
+      //         target="_blank"
+      //         rel="noopener noreferrer"
+      //       >
+      //         Learn React
+      //       </a>
+      //     </header>
+      //   </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={LoginComponent()}/>
+          <Route path="/home" element={<p>HOME PAGE</p>}/>
+        </Routes>
+      </BrowserRouter>
+
+    );
+
+  }
+
+
+
 
 export default App;
---
+++ src/components/LoginComponent/LoginComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import styles from './LoginComponent.module.css';
+
+const LoginComponent = () => {
+  return (
+    <div className={styles.LoginComponent} data-testid="LoginComponent">
+      LOGIN HERE
+    </div>
+  )
+};
+
+
+
+export default LoginComponent;
