d95939e7fa08f37a4a5139fd305bf187647f1369
Fieraol Shiferaw <fieraolshiferaw@fieraols-mbp.myfiosgateway.com>
2022-07-14 14:21:37 -0400

    signin/up

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginComponent from './components/LoginComponent/LoginComponent';
-
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp';
 
 const App = () => {
-    return (
-      //   <div className="App">
-      //     <header className="App-header">
-      //       <img src={logo} className="App-logo" alt="logo" />
-      //       <p>
-      //         PagMan juicer.
-      //       </p>
-      //       <a
-      //         className="App-link"
-      //         href="https://reactjs.org"
-      //         target="_blank"
-      //         rel="noopener noreferrer"
-      //       >
-      //         Learn React
-      //       </a>
-      //     </header>
-      //   </div>
-      <BrowserRouter>
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <BrowserRouter>
         <Routes>
-          <Route path="/login" element={LoginComponent()}/>
-          <Route path="/home" element={<p>HOME PAGE</p>}/>
+          <Route path="/signin" element={SignIn()}/>
+          <Route path="/signup" element={SignUp()}/>
         </Routes>
       </BrowserRouter>
-
-    );
-
-  }
-
-
-
+  );
+}
 
 export default App;
